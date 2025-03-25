acb86bd66470c5aa30308a8a0ac4772ea166be3b
Oluwafemi David <davidoluwafemi178@gmail.com>
2025-03-25 02:00:03 +0100

    add little sticky header

--- app/ui/header/index.tsx
+++ app/ui/header/index.tsx
@@ -1,7 +1,28 @@
+'use client'
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
 export default function Header() {
-    return <header className="bg-white md:bg-[#F3F3F3]">
+    const [isSticky, setIsSticky] = useState(false);
+
+    useEffect(() => {
+        let lastScrollY = window.scrollY;
+        let ticking = false;
+
+        const handleScroll = () => {
+        if (!ticking) {
+            requestAnimationFrame(() => {
+            setIsSticky(window.scrollY > 2000);
+            ticking = false;
+            });
+            ticking = true;
+        }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+    return <header className={`${isSticky? 'fixed top-0 left-0 w-full': ''} transition-all duration-500 ease-in-out bg-white md:bg-[#F3F3F3] z-50`}>
         <Navbar />
     </header>
 }
