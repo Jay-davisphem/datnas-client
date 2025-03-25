d8d1daf5436a1cfda5ae59db197e3ba5eb6265e4
Oluwafemi David <davidoluwafemi178@gmail.com>
2025-03-25 10:04:16 +0100

    cool animation

--- app/ui/body/home/AboutUsItem.tsx
+++ app/ui/body/home/AboutUsItem.tsx
@@ -20,15 +20,15 @@ export default function AboutUsItem({
   return (
     <motion.div 
       className="md:w-1/3 flex flex-col gap-1 items-center text-center p-8 md:py-2 md:px-6 md:not-last:border-r"
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, ease: "easeOut" }}
-      whileHover={{ scale: 1.03 }}
+      initial={{ opacity: 0, y: 20, scale: 0.9, rotate: -5 }}
+      whileInView={{ opacity: 1, y: 0, scale: 1, rotate: 0 }}
+      viewport={{ once: true, amount: 0.3 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
     >
       <Image 
         className="object-cover w-35 h-35 md:w-40 md:h-40 rounded-full border-8 border-[#001A50]" 
         src={imgUrl} 
-        alt={name + profession + school} 
+        alt={`${name} ${profession} ${school}`} 
         width={30} 
         height={30} 
       />
