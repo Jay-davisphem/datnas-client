0c63d20219ac5dad2d510eee50d399e679d1c129
Oluwafemi David <davidoluwafemi178@gmail.com>
2025-03-25 10:50:25 +0100

    fix the header animation

--- app/ui/header/index.tsx
+++ app/ui/header/index.tsx
@@ -1,31 +1,35 @@
 'use client'
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import Navbar from "./Navbar";
 
 export default function Header() {
-    const [isSticky, setIsSticky] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
 
-    useEffect(() => {
-        let lastScrollY = window.scrollY;
-        let ticking = false;
+  useEffect(() => {
+    let ticking = false;
 
-        const handleScroll = () => {
-            if (!ticking) {
-                requestAnimationFrame(() => {
-                    setIsSticky(window.scrollY > 2000);
-                    ticking = false;
-                });
-                ticking = true;
-            }
-        };
+    const handleScroll = () => {
+      if (!ticking) {
+        requestAnimationFrame(() => {
+          setIsSticky(window.scrollY > 2000);
+          ticking = false;
+        });
+        ticking = true;
+      }
+    };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-    return (
-        <header className={`${isSticky ? 'fixed opacity-100' : ''} top-0 left-0 w-full transition-opacity duration-1000 ease-in-out bg-white md:bg-[#F3F3F3] z-50`}>
-            <Navbar />
-        </header>
-    )
+  return (
+    <motion.header
+      layout
+      transition={{ layout: { duration: 2, ease: "easeInOut" } }}
+      className={`${isSticky ? 'fixed opacity-100' : ''} top-0 left-0 w-full transition-opacity duration-1000 ease-in-out bg-white md:bg-[#F3F3F3] z-50`}
+    >
+      <Navbar />
+    </motion.header>
+  );
 }
