49d7b21aebc2833805c7842c0a883fb35b270583
Oluwafemi David <davidoluwafemi178@gmail.com>
2025-03-30 19:18:51 +0100

    trying fix

--- app/ui/footer/Socials.tsx
+++ app/ui/footer/Socials.tsx
@@ -6,20 +6,20 @@ export default function Socials() {
   return (
     <div className="flex flex-col gap-3">
         <div className="flex gap-4 cursor-pointer">
-            <Link href={process.env.NEXT_PUBLICX_HANDLE!} target="_blank">
+            <Link href={process.env.NEXT_PUBLIC_X_HANDLE!} target="_blank">
                 <FaXTwitter className="hover:shadow hover:shadow-white"/>
             </Link>
-            <Link href={process.env.NEXT_PUBLICFB_HANDLE!} target="_blank">
+            <Link href={process.env.NEXT_PUBLIC_FB_HANDLE!} target="_blank">
                 <FaFacebook className="hover:shadow hover:shadow-white"/>
             </Link>
-            <Link href={process.env.NEXT_PUBLICIN_HANDLE!}  target="_blank">
+            <Link href={process.env.NEXT_PUBLIC_IN_HANDLE!}  target="_blank">
                 <FaInstagram className="hover:shadow hover:shadow-white"/>
             </Link>
-            <Link href={process.env.NEXT_PUBLICLK_HANDLE!} target="_blank">
+            <Link href={process.env.NEXT_PUBLIC_LK_HANDLE!} target="_blank">
                 <FaLinkedin className="hover:shadow hover:shadow-white"/>
             </Link>
         </div>
-        <p>Email: <Link href={`mailto:${process.env.NEXT_PUBLICCONTACT_MAIL!}`} className="hover:underline active:underline">{process.env.NEXT_PUBLICCONTACT_MAIL!}</Link></p>
+        <p>Email: <Link href={`mailto:${process.env.NEXT_PUBLIC_CONTACT_MAIL!}`} className="hover:underline active:underline">{process.env.NEXT_PUBLIC_CONTACT_MAIL!}</Link></p>
     </div>
   )
 }
